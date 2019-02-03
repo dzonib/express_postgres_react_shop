b3f4f16c752501c7ca182c81aac43f76fd56539a
dzonib <nikolabosnjak381@gmail.com>
2019-02-03 15:56:54 +0100

    deploying yoyo

--- db/database.js
+++ db/database.js
@@ -1,5 +1,13 @@
 import Sequelize from 'sequelize'
 
-const sequelize = new Sequelize('postgres://postgres:123456@localhost:5433/node-react-shop')
+const sequelize = new Sequelize("d7fdperub8chb1", "nbmebryhqtjjll", "f856238e7ef4be99c5216e0ce54a63d36e1cee5511febbe34c9efe9de3e14e1e", {
+    host: "ec2-46-137-170-51.eu-west-1.compute.amazonaws.com",
+    dialect: "postgres",
+    protocol: 'postgres',
+    
+    dialectOptions: {
+        ssl: true
+    }
+})
 
 export default sequelize
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import path from 'path'
 
 import productRoutes from './routes/admin/product'
 import shopRoutes from './routes/shop/product'
@@ -44,5 +45,16 @@ Product.belongsToMany(Cart, { through: CartItem })
 Order.belongsTo(User)
 User.hasMany(Order)
 Order.belongsToMany(Product, { through: OrderItem })
+
+// Serve static assets if in production
+
+if (process.env.NODE_ENV === 'production') {
+    // set static folder 
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 sequelize.sync().then(() => app.listen(port, () => console.log(`App running on http://localhost:${port}`)))
